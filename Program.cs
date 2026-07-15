e6108b1fa86251738fb4105965e0ade184d3ea2d
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-07-15 09:05:14 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int pago, precio;
+        string producto;
+        Console.WriteLine("Nombre: Tiffany Godoy Quex\nGrado: IV C\nClave: 12");
+        Console.WriteLine("Escriba el nombre del producto: ");
+        producto = Console.ReadLine();
+        Console.Write("Digite el precio del producto: ");
+        precio = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digite el valor del billete: ");
+        pago = Convert.ToInt32(Console.ReadLine());
+        int cambio = pago - precio;
+        Console.WriteLine("Su cambio es: "+cambio);
+    }
+}
